437700fe248cd1b74daac818e863e898805db03d
Pedro Silva <DarthPedro@gmail.com>
2025-03-07 12:35:57 -0800

    Moved auth0 service configuration to DependencyInjection.AddAuth function.

--- src/HabitTracker.Api/DependencyInjection.cs
+++ src/HabitTracker.Api/DependencyInjection.cs
@@ -1,9 +1,40 @@
 ﻿using HabitTracker.Api.Features.Categories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HabitTracker.Api;
 
 internal static class DependencyInjection
 {
+    private const string _authDomain = "Auth0:Domain";
+    private const string _authAudience = "Auth0:Audience";
+
+    public static WebApplicationBuilder AddAuth(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, c =>
+            {
+                c.Authority = $"https://{builder.Configuration[_authDomain]}";
+                c.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidAudience = builder.Configuration[_authAudience],
+                    ValidIssuer = $"https://{builder.Configuration[_authDomain]}",
+                };
+            });
+
+        builder.Services.AddAuthorization();
+
+        builder.Services.AddCors(options =>
+            options.AddDefaultPolicy(policy =>
+            {
+                policy.AllowAnyOrigin();
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+            }));
+
+        return builder;
+    }
+
     public static WebApplication ConfigurePipeline(this WebApplication app)
     {
         // Configure the HTTP request pipeline.
--- src/HabitTracker.Api/Program.cs
+++ src/HabitTracker.Api/Program.cs
@@ -1,32 +1,11 @@
 using HabitTracker.Api;
 using HabitTracker.Api.Features;
 using HabitTracker.Api.Persistence;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args)
                             .AddDatabase()
-                            .AddServices();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, c =>
-    {
-        c.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
-        c.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-        {
-            ValidAudience = builder.Configuration["Auth0:Audience"],
-            ValidIssuer = $"https://{builder.Configuration["Auth0:Domain"]}"
-        };
-    });
-
-builder.Services.AddAuthorization();
-
-builder.Services.AddCors(options =>
-    options.AddDefaultPolicy(policy =>
-        {
-            policy.AllowAnyOrigin();
-            policy.AllowAnyHeader();
-            policy.AllowAnyMethod();
-        }));
+                            .AddServices()
+                            .AddAuth();
 
 builder.Build()
        .ConfigurePipeline()
