d130630dad79112d2dc0de5d88ff65588fa9cc46
Pedro Silva <DarthPedro@gmail.com>
2025-03-07 11:23:47 -0800

    Added support to HabitTracker web app for authentication with Auth0. Added shared components for Blazor auth. Added appsettings for Auth0 configuration.

--- src/HabitTracker.Web/Program.cs
+++ src/HabitTracker.Web/Program.cs
@@ -8,4 +8,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(" https://localhost:7050") });
 
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+
 await builder.Build().RunAsync();
