b973914c2b4b57c8af7b9011ec5e70f85e83f045
Pedro Silva <DarthPedro@gmail.com>
2025-03-07 12:14:52 -0800

    Changed web app setup to send http requests with appropriate bearer access token to webapi. Added CustomAuthorizationMessageHandler to do this work.

---
+++ src/HabitTracker.Web/CustomAuthorizationMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace HabitTracker.Web;
+
+public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
+{
+    public CustomAuthorizationMessageHandler(IAccessTokenProvider provider, NavigationManager navigation)
+        : base(provider, navigation)
+    {
+        // Set the specific API URL
+        ConfigureHandler(
+            authorizedUrls: ["https://localhost:7050"],
+            scopes: ["https://habit-track-api"]
+        );
+    }
+}
--- src/HabitTracker.Web/Program.cs
+++ src/HabitTracker.Web/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using HabitTracker.Web;
 
@@ -6,12 +7,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(" https://localhost:7050") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri("https://localhost:7050"))
+    .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ServerAPI"));
 
 builder.Services.AddOidcAuthentication(options =>
 {
     builder.Configuration.Bind("Auth0", options.ProviderOptions);
     options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"] ?? "");
 });
 
 await builder.Build().RunAsync();
