d12c06d0a2c3a82f300adadf48c5b60b0f5a8341
Pedro Silva <DarthPedro@gmail.com>
2025-03-18 16:54:15 -0700

    Update NavMenu to auto expand Reports tab when a subitem is navigated to. Moved logic to NavMenu code behind file. Added initial today/weekly/monthly report routes.

---
+++ src/HabitTracker.Web/Layout/NavMenu.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace HabitTracker.Web.Layout;
+
+public partial class NavMenu
+{
+    private bool collapseNavMenu = true;
+    private bool isReportsExpanded = false;
+
+    private void ToggleNavMenu() => collapseNavMenu = !collapseNavMenu;
+
+    private void ToggleReports() => isReportsExpanded = !isReportsExpanded;
+
+    protected override void OnInitialized()
+    {
+        Navigation.LocationChanged += OnLocationChanged;
+        UpdateReportsExpanded();
+    }
+
+    private void OnLocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        UpdateReportsExpanded();
+        InvokeAsync(StateHasChanged);
+    }
+
+    private void UpdateReportsExpanded()
+    {
+        var currentUri = Navigation.Uri.ToLower();
+        var isReportLink = currentUri.Contains("/report/");
+        if (isReportLink is true) isReportsExpanded = true;
+    }
+
+    public void Dispose()
+    {
+        Navigation.LocationChanged -= OnLocationChanged;
+    }
+}
