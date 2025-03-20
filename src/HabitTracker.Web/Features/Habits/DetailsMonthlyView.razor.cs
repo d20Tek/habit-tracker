07dfd2ff52af27ed39e1fac329440135e0dedc09
Pedro Silva <DarthPedro@gmail.com>
2025-03-20 11:51:43 -0700

    Implemented DetailsMonthlyView to show in Details page (monthly tab).

---
+++ src/HabitTracker.Web/Features/Habits/DetailsMonthlyView.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace HabitTracker.Web.Features.Habits;
+
+public partial class DetailsMonthlyView
+{
+    private Option<string> _errorMessage = Option<string>.None();
+    private Option<HabitResponse> _habit = Option<HabitResponse>.None();
+
+    [Parameter]
+    public int HabitId { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var fullUrl = Constants.Habits.ServiceUrlWithLimit(HabitId, Constants.Habits.LimitMonthly);
+        await _http.TryGetByIdFromJsonAsync<HabitResponse>(fullUrl, _log)
+                   .HandleResultAsync(h => _habit = h, e => _errorMessage = e);
+    }
+}
