bab4a7e81a60e3e35d5f4b58174c3197fef36f9f
Pedro Silva <DarthPedro@gmail.com>
2025-03-25 11:09:05 -0700

    Added EF migration for ContentLink model class.

--- src/HabitTracker.Api/Persistence/Migrations/20250325180746_Add-ContentLink-Model.Designer.cs
+++ src/HabitTracker.Api/Persistence/Migrations/20250325180746_Add-ContentLink-Model.Designer.cs
@@ -4,6 +4,7 @@ using HabitTracker.Api.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HabitTracker.Api.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class HabitTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250325180746_Add-ContentLink-Model")]
+    partial class AddContentLinkModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
