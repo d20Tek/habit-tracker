d4b0f0550abc8ab82e8bca810133a1ee309b97ee
Pedro Silva <DarthPedro@gmail.com>
2025-03-26 21:31:03 -0700

    Created a Donate button that links to ko-fi page.

--- src/HabitTracker.Web/Common/Constants.cs
+++ src/HabitTracker.Web/Common/Constants.cs
@@ -6,6 +6,8 @@ internal static partial class Constants
 
     public const string DefaultErrorCode = "ServiceApi";
 
+    public const string DonationUrl = "https://ko-fi.com/V7V8UGPMB";
+
     public const string UnexpectedServiceMessage = "Unexpected server error from backend service.";
 
     public static Error UnexpectedServiceError(string code) => Error.Unexpected(code, UnexpectedServiceMessage);
