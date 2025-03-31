7ead26fe709f23b3c2c248c1ebf0c61461f894fa
Pedro Silva <DarthPedro@gmail.com>
2025-03-30 20:08:43 -0700

    Created LoadingButton component to show a spinner if the OnClick action takes longer than specified delay. Changed Mark/Unmark buttons on MarkHabitsComponent to use LoadingButtons. Changed ListWeighings page to use LoadingButton.

---
+++ src/HabitTracker.Web/Common/LoadingButton.razor.cs
@@ -0,0 +1,50 @@
+﻿namespace HabitTracker.Web.Common;
+
+public partial class LoadingButton
+{
+    [Parameter]
+    public string IconClass { get; set; } = "bi bi-play";
+
+    [Parameter]
+    public string Label { get; set; } = string.Empty;
+
+    [Parameter]
+    public EventCallback OnClick { get; set; }
+
+    [Parameter]
+    public int SpinnerDelay { get; set; } = 100;
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string, object>? AdditionalAttributes { get; set; }
+
+    private bool _isLoading = false;
+
+    private async Task OnClickAsync()
+    {
+        // prevent multiple clicks while operation is running.
+        if (_isLoading) return;
+
+        try
+        {
+            var delayTask = Task.Delay(SpinnerDelay);
+            var clickTask = OnClick.InvokeAsync();
+
+            // wait for the first task to complete.
+            await Task.WhenAny(delayTask, clickTask);
+
+            // if the OnClick event callback hasn't completed yet, then wait for it.
+            if (!clickTask.IsCompleted)
+            {
+                _isLoading = true;
+                StateHasChanged();
+
+                await clickTask;
+            }
+        }
+        finally
+        {
+            _isLoading = false;
+            StateHasChanged();
+        }
+    }
+}
