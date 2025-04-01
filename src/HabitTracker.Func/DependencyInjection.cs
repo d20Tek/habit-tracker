7856fd734a1dc00d98fa78f954c9c8eeb8ef20b9
Pedro Silva <DarthPedro@gmail.com>
2025-04-01 15:33:36 -0700

    Added Azure Functions project to solution that will regularly ping Habits database and service. Ensured configuration of database and httpclient is correct. Implemented CheckHabitsDb function that uses EF to perform a simple query on the database. Implmented CheckHabitsServices function that verifies an unauthenticated service call.

---
+++ src/HabitTracker.Func/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using HabitTracker.Func.CheckHabits;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HabitTracker.Func;
+
+internal static class DependencyInjection
+{
+    private const string _habitsDbConnection = "HabitsDbConnection";
+    private const string _habitsServiceUrl = "ServiceUrls:HabitsService";
+    private const string _habitsServiceName = "HabitsService";
+
+    public static IServiceCollection ConfigureHabitsRequirements(
+        this IServiceCollection services,
+        HostBuilderContext host)
+    {
+        string connectionString = host.Configuration.GetConnectionString(_habitsDbConnection)
+                                      ?? throw new InvalidOperationException("Habits SQL connection string is missing.");
+
+        services.AddDbContext<HabitsDbContext>(options => options.UseSqlServer(connectionString));
+
+        string serviceUrl = host.Configuration[_habitsServiceUrl]
+                                ?? throw new InvalidOperationException("Habits SQL connection string is missing.");
+
+        services.AddHttpClient(_habitsServiceName, client => client.BaseAddress = new(serviceUrl));
+
+        return services;
+    }
+}
